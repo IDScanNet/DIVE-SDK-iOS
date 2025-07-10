fc2beba08894ab3b61b94b8d7973ab20ba09ab3f
Alexander Korotkov <akorotkov@abyka.com>
2025-07-10 12:12:59 +0500

    - added PDF support for new documents

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/IDScanNet/IDScanIDDetectorIOS.git", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/IDScanNet/IDScanToolsIOS.git", .upToNextMajor(from: "1.0.0"))
+        .package(url: "https://github.com/IDScanNet/IDScanToolsIOS.git", .upToNextMajor(from: "2.0.0"))
     ],
     targets: [
         .target(
